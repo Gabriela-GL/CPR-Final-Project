d63413ebfd8eca1982ca7c6a471101b1438a367c
Gabriela-GL <130388149+Gabriela-GL@users.noreply.github.com>
2023-04-12 19:50:33 -0400

    Update converting.h
    updated header file converting

--- Final-Project-Bb02/converting.h
+++ Final-Project-Bb02/converting.h
@@ -1,5 +1,16 @@
 // CONVERTING MODULE HEADER
 
+/*
+
+Programmer: Jorel Rutger
+Last update of code: April 12, 2023
+
+Module Functionality:
+	Version 1: Obtains a string and converts it into an integer.
+	Version 2: Obtains a string and converts it into a double.
+
+*/
+
 #ifndef _CONVERTING_H_
 #define _CONVERTING_H_
 
