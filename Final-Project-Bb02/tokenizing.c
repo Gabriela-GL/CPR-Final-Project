3e513020154c76551daba8eeb6555e9ab0fa26e1
Gabriela-GL <130388149+Gabriela-GL@users.noreply.github.com>
2023-04-12 16:16:14 -0400

    Update tokenizing.c
    tokenizing v2 and v3 upgraded

--- Final-Project-Bb02/tokenizing.c
+++ Final-Project-Bb02/tokenizing.c
@@ -5,8 +5,10 @@
 
 void tokenizing(void)
 {
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 	/* Version 1 >> insert here
-	*/
+	
 	printf("*** Start of Tokenizing Words Demo ***\n");
 	char words[BUFFER_SIZE]; //word will be used to save user input
 	char* nextWord = NULL; // nextword will be used to find the spaces between word to separate it before the output
@@ -32,13 +34,70 @@ void tokenizing(void)
 
 	} while (strcmp(words, "q") != 0); //exit condition from the principal loop
 	printf("*** End of Tokenizing Words Demo ***\n\n"); //bye message
+	*/
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
-}
-
-/* Version 2
+	/* Version 2
 >> insert here
+
+
+	printf("*** Start of Tokenizing Phrases Demo ***\n");
+	char phrases[BUFFER_SIZE]; // will be used to save user input
+	char* nextPhrase = NULL; // will be used to find the separator between the phrases
+	int phrasesCounter = 0; // will be used to count the total of phrases
+
+	do // conditional loop to check and output the number of phrases and which phrase is (user input)
+	{
+		printf("Type a few phrases separated by comma (q - to quit): \n");
+		fgets(phrases, BUFFER_SIZE, stdin); // receives input from user
+		phrases[strlen(phrases) - 1] = '\0'; // assingned the non-terminator value for string librarie tools
+
+		if (strcmp(phrases, "q") != 0) // condition to check if the user want to exit of the programm execution
+		{
+			nextPhrase = strtok(phrases, ","); //find comma between phrases to count and separate it
+			phrasesCounter = 1;
+
+			while (nextPhrase) // loop to print the program results
+			{
+				printf("Phrase #%d is \'%s\'\n", phrasesCounter++, nextPhrase);
+				nextPhrase= strtok(NULL, ",");
+			}
+		}
+
+	} while (strcmp(phrases, "q") != 0); //exit condition from the principal loop
+	printf("*** End of Tokenizing Phrases Demo ***\n\n"); //bye message
 */
+
 
 /* Version 3
 >> insert here
 */
+
+	printf("*** Start of Tokenizing sentences Demo ***\n");
+	char sentences[BUFFER_SIZE]; // will be used to save user input
+	char* nextSentence = NULL; // will be used to find the separator between the sentences
+	int sentenceCounter; // will be used to count the total of sentences
+
+	do // conditional loop to check and output the number of sentences and which sentence is (user input)
+	{
+		printf("Type a few phrases separated by comma (q - to quit): \n");
+		fgets(sentences, BUFFER_SIZE, stdin); // receives input from user
+		sentences[strlen(sentences) - 1] = '\0'; // assingned the non-terminator value for string librarie tools
+
+		if (strcmp(sentences, "q") != 0) // condition to check if the user want to exit of the programm execution
+		{
+			nextSentence = strtok(sentences, "."); //find point between sentence to count and separate it
+			sentenceCounter = 1;
+
+			while (nextSentence) // loop to print the program results
+			{
+				printf("Sentence #%d is \'%s\'\n", sentenceCounter++, nextSentence);
+				nextSentence = strtok(NULL, ".");
+			}
+		}
+
+	} while (strcmp(sentences, "q") != 0); //exit condition from the principal loop
+	printf("*** End of Tokenizing sentence Demo ***\n\n"); //bye message
+
+}
