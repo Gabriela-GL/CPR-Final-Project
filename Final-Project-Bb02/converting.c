a22aeb3473e7ac0c60d9cd9a99af2d85c3ad4d41
Gabriela-GL <130388149+Gabriela-GL@users.noreply.github.com>
2023-04-12 19:48:05 -0400

    Update converting.c
    converting code v2 added

--- Final-Project-Bb02/converting.c
+++ Final-Project-Bb02/converting.c
@@ -1,4 +1,16 @@
 // CONVERTING MODULE SOURCE
+
+/*
+
+Programmer: Jorel Rutger
+Last update of code: April 12, 2023
+
+Module Functionality:
+	Version 1: Obtains a string and converts it into an integer.
+	Version 2: Obtains a string and converts it into a double.
+
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 80
 
@@ -7,7 +19,7 @@
 
 void converting(void) {
 
-	/* Start of Version 1 */
+	/* V1: Start of Version 1 */
 	printf("*** Start of Converting Strings to int Demo ***\n");
 
 	char	intString[BUFFER_SIZE]; // Buffers holding user input string
@@ -16,6 +28,7 @@ void converting(void) {
 	do {
 		// Input Prompt
 		printf("Please enter an integer numeric string (or 'q' to quit):\n");
+
 		fgets(intString, BUFFER_SIZE, stdin); // Reads input string from the user
 		intString[strlen(intString) - 1] = '\0'; // Removes newline character at the end of input string
 
@@ -32,9 +45,27 @@ void converting(void) {
 	/* End of Version 1 */
 
 
+	/* V2: Start of Version 2 */
+	printf("*** Start of Converting Strings to double Demo ***\n");
+	char	doubleString[BUFFER_SIZE]; // Buffers holding user input string
+	double	doubleNumber; // A variable to store a converted double
 
-/* Version 2 */
+	do {
+		// Input Prompt
+		printf("Type the double numeric string (q - to quit):\n");
 
+		fgets(doubleString, BUFFER_SIZE, stdin); // Reads input string from the user
+		doubleString[strlen(doubleString) - 1] = '\0'; // Removes newline character at the end of input string
+
+		if ((strcmp(doubleString, "q") != 0)) { // Checking if input string is not equal to "q"
+			// Conversion and Output
+			doubleNumber = atof(doubleString); // Converts input string to double
+			printf("Converted number is %f\n", doubleNumber); // Displays the converted double
+		}
+	} while (strcmp(doubleString, "q") != 0); // Continue looping until input string is "q"
+
+	printf("*** End of Converting Strings to double Demo ***\n\n");
+	/* End of Version 2 */	
 
 
 /* Version 3 */
